76bd0c077382398b6773d801a8f33d6d4793dbfd
Shariful Islam <115772378+https-sharif@users.noreply.github.com>
2024-12-01 00:29:30 +0600

    Update solution.cpp

--- 2097. Valid Arrangement of Pairs/solution.cpp
+++ 2097. Valid Arrangement of Pairs/solution.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
+    map<int, vector<int>> adj;
+    map<int, int> in, out;
+    vector<vector<int>> ans;
+
+    void euler(int u) {
+        while(!adj[u].empty()) 
+        {
+            int v = adj[u].back();
+            adj[u].pop_back();
+            euler(v);
+            ans.push_back({u, v});
+        }
+    }
+
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        
+        for(auto it : pairs) 
+        {
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            out[u]++;
+            in[v]++;
+        }
+        int start = -1;
+        for(auto [it, _] : adj) 
+        {
+            if(out[it] > in[it]) 
+            {
+                start = it;
+                break;
+            }
+        }
+        if(start == -1) start = pairs[0][0];
+        euler(start);
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
